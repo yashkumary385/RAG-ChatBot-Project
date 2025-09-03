44bc7c169e7380687102fe8df17e9ad39426f67e
yashkumary385 <114864924+yashkumary385@users.noreply.github.com>
2025-09-03 20:35:07 +0530

    setup project and defined document schema

---
+++ server/models/DocumentSchema.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const documentSchema = new mongoose.Schema({
+title:{
+    type:String,
+    required:true,
+    trim:true
+},
+originalName:{
+    type:String,
+    required:true,
+},
+content:{
+    type:String,
+    required:true,
+},
+fileSize:{
+    type:Number,
+    required:true,
+},
+mimeType:{
+    type:String,
+    required:true,
+},uploadPath:{
+    type:String,
+    required:true,      
+},
+processingState:{
+    type:String,
+    enum: ['pending', 'processing', 'completed', 'failed'],  
+    default:"pending"
+},
+chunkCount:{
+    type:Number,
+    default:0   },
+    metaData:{
+        pageCount:Number,   
+        chunkCount:Number,
+        language:String,
+        extractedDate:Date
+    },
+      createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+},{
+    timestamps:true
+})
+
+documentSchema.index({ title: 1 }); // for fast searching
+documentSchema.index({ createdAt: -1 });
+documentSchema.index({ processingStatus: 1 });
+export default mongoose.model("Document",documentSchmea)
---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors"
+import path from "path"
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(import.meta.url);
+// // file:///C:/Users/ASUS/project/server.js
+
+// console.log(fileURLToPath(import.meta.url));
+// // C:\Users\ASUS\project\server.js
+
+
+import ConnectDb  from "./config/db.js";
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use('uploads', express.static(path.join(__dirname, 'uploads')))
+
+// health check Route
+app.get('/health', (req, res) => {
+  res.json({ 
+    status: 'OK', 
+    message: 'RAG API Server is running',
+    timestamp: new Date().toISOString()
+  });
+});
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello we working yes sirrr")
+})
+
+ConnectDb()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`);
+    
+})
